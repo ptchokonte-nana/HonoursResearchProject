a4c52bc46044f1eea038f4d06a06228814bf1f2d
ptchokonte-nana <110400031+ptchokonte-nana@users.noreply.github.com>
2022-08-10 16:18:40 +0200

    Update analyse_data.R
    impute function added

--- analyse_data.R
+++ analyse_data.R
@@ -13,3 +13,25 @@ plt_data <- function(data){
 }
 
 
+# impute missing (N/A) values in data --------------------------------------
+
+imp_data <- function(data){
+  plt_data <- data[,4:28]
+  plt_data <- missForest(plt_data, ntree=200, decreasing=TRUE)$ximp
+}
+
+
+# check for a value that is zero in the imputed data -----------------------
+
+check_data <- function(data){
+  lst <- c() #empty list
+  for (k in 4:ncol(data)) {
+    for (l in 1:nrow(data)) {
+      if (data[l,k]==0.0){
+        lst <- append(lst, c(colnames(plt_data[k]),l))
+      }
+    }
+  }
+  return(lst)  #returns the column name and row number
+}
+
