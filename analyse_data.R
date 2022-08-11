07f599ab109eda8be8dffab528f313d8d1c91c8f
ptchokonte-nana <110400031+ptchokonte-nana@users.noreply.github.com>
2022-08-11 14:26:45 +0200

    Update analyse_data.R

--- analyse_data.R
+++ analyse_data.R
@@ -1,15 +1,17 @@
 # plot box and whiskers ---------------------------------------------------
 
 plt_data <- function(data){
+  data[, 7:28] <- scale(data[,7:28], center = TRUE, scale = TRUE)
   fmt_long_data <- pivot_longer(data, 
                                 7:28, 
                                 names_to = "Biomarkers",
                                 values_to = "Observations")
   
-  print(ggplot(fmt_long_data, aes(x=TB_status, y=log10(Observations))) + 
-    geom_boxplot() +
-    facet_wrap("Biomarkers") + 
-    ylab("Concentration"))
+  print(ggplot(fmt_long_data, aes(x=TB_status, y=log10(Observations))) +
+          geom_boxplot() +
+          facet_wrap("Biomarkers") + 
+          ylab("log10"))  
+  return(data)
 }
 
 
