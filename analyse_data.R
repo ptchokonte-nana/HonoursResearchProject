42fbd0aa4d55a62b29cca789c14a96dad0d36598
ptchokonte-nana <110400031+ptchokonte-nana@users.noreply.github.com>
2022-08-17 12:18:07 +0200

    Update analyse_data.R

--- analyse_data.R
+++ analyse_data.R
@@ -1,7 +1,7 @@
 # plot box and whiskers ---------------------------------------------------
 
 plt_data <- function(data){
-  data[, 7:28] <- scale(data[,7:28], center = TRUE, scale = TRUE)
+  data[, 7:28] <- scale(data[, 7:28], center = TRUE, scale = TRUE)
   fmt_long_data <- pivot_longer(data, 
                                 7:28, 
                                 names_to = "Biomarkers",
@@ -9,31 +9,9 @@ plt_data <- function(data){
   
   print(ggplot(fmt_long_data, aes(x=TB_status, y=log10(Observations))) +
           geom_boxplot() +
-          facet_wrap("Biomarkers") + 
+          facet_wrap("Biomarkers", ncol = 6) + 
           ylab("log10"))  
   return(data)
 }
 
-
-# impute missing (N/A) values in data --------------------------------------
-
-imp_data <- function(data){
-  plt_data <- data[,4:28]
-  plt_data <- missForest(plt_data, ntree=200, decreasing=TRUE)$ximp
-}
-
-
-# check for a value that is zero in the imputed data -----------------------
-
-check_data <- function(data){
-  lst <- c() #empty list
-  for (k in 4:ncol(data)) {
-    for (l in 1:nrow(data)) {
-      if (data[l,k]==0.0){
-        lst <- append(lst, c(colnames(plt_data[k]),l))
-      }
-    }
-  }
-  return(lst)  #returns the column name and row number
-}
 
