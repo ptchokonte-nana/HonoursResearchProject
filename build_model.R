a61be3b550281a502cbf34af72d673c5b9182b26
ptchokonte-nana <110400031+ptchokonte-nana@users.noreply.github.com>
2022-10-28 19:12:07 +0200

    Delete build_model.R

--- build_model.R
+++
@@ -1,9 +0,0 @@
-# split data into training and test set -----------------------------------
-
-split_data <- function(data){
-  sample_data <- sample(data, nrow(data)) #shuffle data
-  sample_data <- sample(2, nrow(sample_data), replace = T, prob = c(0.7,0.3))
-  return(sample_data)
-}
-
-
